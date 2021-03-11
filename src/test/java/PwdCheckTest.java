3342ba16b0f097f943fe4546c6dae7222c2e8f4e
Daniel Kleissl <daniel.kleissl@gmx.at>
2021-03-11 20:43:05 +0100

    refactored testcases, single assert statements

--- src/test/java/PwdCheckTest.java
+++ src/test/java/PwdCheckTest.java
@@ -1,7 +1,4 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +21,7 @@ class PwdCheckTest {
     private String pwDisallowedSpecialChars;
     private String pwNumbersAndChars;
     private String pwOnlyNums;
+    private Handler check;
 
     @BeforeAll
     static void init() {
@@ -47,96 +45,220 @@ class PwdCheckTest {
 
     }
 
-    @DisplayName("Test")
-    @Test
-    void testLength() {
-
-        Handler check = new LengthCheckHandler();
-
-        assertFalse(check.handle(pwTooShort));
-        assertFalse(check.handle(pwTooLong));
-        assertTrue(check.handle(pwOk));
-
+    @AfterEach
+    void close() {
+        check = null;
     }
 
+    @DisplayName("Password too short")
+    @Test
+    void testLength_TooShort() {
+        Handler check = new LengthCheckHandler();
+        assertFalse(check.handle(pwTooShort));
+    }
+
+    @DisplayName("Password too long")
+    @Test
+    void testLength_TooLong() {
+        check = new LengthCheckHandler();
+        assertFalse(check.handle(pwTooLong));
+    }
+
+    @DisplayName("Password has correct length")
+    @Test
+    void testLength_Ok() {
+        check = new LengthCheckHandler();
+        assertTrue(check.handle(pwOk));
+    }
+
+    @DisplayName("Password is null")
+    @Test
+    void testPwIsNull() {
+        check = new LengthCheckHandler();
+        assertFalse(check.handle(pwIsNull));
+    }
+
+    @DisplayName("Password not null")
     @Test
     void pwNotNull() {
-
-        Handler check = new LengthCheckHandler();
-
-        assertFalse(check.handle(pwIsNull));
+        check = new LengthCheckHandler();
         assertTrue(check.handle(pwOk));
     }
 
+    @DisplayName("Password has no upper case letters")
     @Test
-    void testContainsUpperCase() {
-
-        Handler check = new UpperCaseCheckHandler();
-
+    void testUpperCaseHandler_noUppercase() {
+        check = new UpperCaseCheckHandler();
         assertFalse(check.handle(pwNoUpperNoSpecial));
-        assertTrue(check.handle(pwOk));
+    }
+
+    @DisplayName("Password has upper case letters but consecutive nums")
+    @Test
+    void testUpperCaseHandler_containsUpperCase() {
+        check = new UpperCaseCheckHandler();
         assertTrue(check.handle(pwConsecutiveNums));
     }
 
+    @DisplayName("Password Ok")
     @Test
-    void testContainsNums() {
-
-        Handler check = new ContainsNumbersCheckHandler();
-
-        assertFalse(check.handle(pwNoUpperNoSpecial));
+    void testUpperCaseHandler_Ok() {
+        check = new UpperCaseCheckHandler();
         assertTrue(check.handle(pwOk));
+    }
+
+    @DisplayName("Password contains no numbers")
+    @Test
+    void testContainsNumsHandler_NoNums() {
+        check = new ContainsNumbersCheckHandler();
+        assertFalse(check.handle(pwNoUpperNoSpecial));
+    }
+
+    @DisplayName("Password contains numbers but is too long")
+    @Test
+    void testContainsNumsHandler_TooLong() {
+        check = new ContainsNumbersCheckHandler();
         assertTrue(check.handle(pwTooLong));
     }
 
+    @DisplayName("Password contains numbers but is too long")
     @Test
-    void testContainsAllowedSpecialChars() {
-
-        Handler check = new ContainsSpecialCheckHandler();
-
-        assertFalse(check.handle(pwNoUpperNoSpecial));
-        assertFalse(check.handle(pwDisallowedSpecialChars));
+    void testContainsNumsHandler_Ok() {
+        check = new ContainsNumbersCheckHandler();
         assertTrue(check.handle(pwOk));
-        assertTrue(check.handle(pwAllowedSpecialChars));
+    }
+
+    @DisplayName("Password contains no special characters")
+    @Test
+    void testSpecialCharsHandler_NoSpecial() {
+
+        check = new ContainsSpecialCheckHandler();
+        assertFalse(check.handle(pwNoUpperNoSpecial));
+    }
+
+    @DisplayName("Password contains no special characters")
+    @Test
+    void testSpecialCharsHandler_NoSpecial2() {
+        check = new ContainsSpecialCheckHandler();
         assertFalse(check.handle(pwNumbersAndChars));
     }
 
+    @DisplayName("Password contains disallowed characters")
     @Test
-    void testContainsNoConsecutiveNums() {
+    void testSpecialCharsHandler_Disallowed() {
 
-        Handler check = new ConsecutiveNumbersCheckHandler();
+        check = new ContainsSpecialCheckHandler();
+        assertFalse(check.handle(pwDisallowedSpecialChars));
+    }
 
-        assertFalse(check.handle(pwConsecutiveNums));
-        assertTrue(check.handle(pwOnlyNums));
-        assertTrue(check.handle(pwNumbersAndChars));
+    @DisplayName("Password contains only allowed special characters")
+    @Test
+    void testSpecialCharsHandler_Allowed() {
+
+        check = new ContainsSpecialCheckHandler();
+        assertTrue(check.handle(pwAllowedSpecialChars));
+    }
+
+    @DisplayName("Password ok")
+    @Test
+    void testSpecialCharsHandler_OK() {
+
+        check = new ContainsSpecialCheckHandler();
         assertTrue(check.handle(pwOk));
+    }
+
+
+
+
+    @DisplayName("Password contains consecutive numbers")
+    @Test
+    void testConsecutiveNumsHandler_ConsecutiveNums() {
+
+        check = new ConsecutiveNumbersCheckHandler();
+        assertFalse(check.handle(pwConsecutiveNums));
+    }
+
+
+    @DisplayName("Password contains only numbers, no consecutives")
+    @Test
+    void testConsecutiveNumsHandler_OnlyNums() {
+
+        check = new ConsecutiveNumbersCheckHandler();
+        assertTrue(check.handle(pwOnlyNums));
+    }
+
+
+    @DisplayName("Password contains no special characters, only numbers and characters, no consecutives")
+    @Test
+    void testConsecutiveNumsHandler_NumbersAndChars() {
+        check = new ConsecutiveNumbersCheckHandler();
+        assertTrue(check.handle(pwNumbersAndChars));
+    }
+
+    @DisplayName("Password contains repeated numbers, no consecutive incrementing numbers")
+    @Test
+    void testConsecutiveNumsHandler_RepeatedNumbers() {
+        check = new ConsecutiveNumbersCheckHandler();
         assertTrue(check.handle(pwRepeatedNums));
     }
 
+    @DisplayName("Password ok")
     @Test
-    void testContainsNoRepeatedNums() {
-
-        Handler check = new RepeatedNumbersCheckHandler();
-
-        assertTrue(check.handle(pwConsecutiveNums));
-        assertFalse(check.handle(pwOnlyNums));
-        assertFalse(check.handle(pwRepeatedNums));
-        assertTrue(check.handle(pwNumbersAndChars));
+    void testConsecutiveNumsHandler_Ok() {
+        check = new ConsecutiveNumbersCheckHandler();
         assertTrue(check.handle(pwOk));
     }
 
+    @DisplayName("Password contains consecutive numbers, no repeats")
     @Test
-    void testChain() {
-        PasswordChecker checker = new PasswordChecker();
+    void testRepeatedNumsHandler_ConsecutiveNums() {
 
+        check = new RepeatedNumbersCheckHandler();
+        assertTrue(check.handle(pwConsecutiveNums));
+    }
+
+    @DisplayName("Password contains only numbers and repeats")
+    @Test
+    void testRepeatedNumsHandler_OnlyNumbers() {
+
+        check = new RepeatedNumbersCheckHandler();
+        assertFalse(check.handle(pwOnlyNums));
+    }
+
+    @DisplayName("Password contains repeated numbers")
+    @Test
+    void testRepeatedNumsHandler_RepeatedNumbers() {
+
+        check = new RepeatedNumbersCheckHandler();
+        assertFalse(check.handle(pwRepeatedNums));
+    }
+
+    @DisplayName("Password contains consecutive numbers and characters, no repeats")
+    @Test
+    void testRepeatedNumsHandler_NumbersAndChars() {
+
+        check = new RepeatedNumbersCheckHandler();
+        assertTrue(check.handle(pwNumbersAndChars));
+    }
+
+    @DisplayName("Password contains consecutive numbers, no repeats")
+    @Test
+    void testRepeatedNumsHandler_Ok() {
+
+        check = new RepeatedNumbersCheckHandler();
+        assertTrue(check.handle(pwOk));
+    }
+
+    @DisplayName("Tests client, chain of responsibility creation and correct return. Password is correct")
+    @Test
+    void testChain_PasswordOk() {
+        PasswordChecker checker = new PasswordChecker();
         assertTrue(checker.checkPassword(pwOk));
-        assertFalse(checker.checkPassword(pwOnlyNums));
-        assertFalse(checker.checkPassword(pwNumbersAndChars));
-        assertFalse(checker.checkPassword(pwRepeatedNums));
+    }
+
+    @DisplayName("Tests client, chain of responsibility creation and correct return. Password is not correct")
+    @Test
+    void testChain_PasswordNotOk() {
+        PasswordChecker checker = new PasswordChecker();
         assertFalse(checker.checkPassword(pwConsecutiveNums));
-        assertFalse(checker.checkPassword(pwTooLong));
-        assertFalse(checker.checkPassword(pwTooShort));
-        assertFalse(checker.checkPassword(pwNoUpperNoSpecial));
-        assertFalse(checker.checkPassword(pwNoSpecial));
-        assertFalse(checker.checkPassword(pwDisallowedSpecialChars));
     }
 }
